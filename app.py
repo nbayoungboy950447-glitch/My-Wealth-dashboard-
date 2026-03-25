6592990b6e9aac2ff1d38e2bda7f89560247c5e0
nbayoungboy950447-glitch <nbayoungboy950447@gmail.com>
2026-03-25 15:23:33 +0900

    Modify send_transaction_email function signature
    Updated send_transaction_email function to include beneficiary and ref_id parameters.

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ bank_data = {
     },
     "history": []
 }
-def send_transaction_email(to_email, user_name, amount, transaction_date):
+def send_transaction_email(to_email, user_fullname, amount, transaction_date,beneficiary,ref_id):
     # Setup Brevo configuration using Render variables
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key['api-key'] = os.environ.get('BREVO_API_KEY')
